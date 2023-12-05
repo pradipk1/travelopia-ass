9cf2d48f230463fab39e49cf7f547349e9bc9267
pradipk1 <pradipshukla12699@gmail.com>
2023-12-05 12:49:04 +0530

    page functionality achieved with responsiveness and readme file also is added!

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# travelopia-ass
+# About this project:
+* This is a static web page, has been developed using CSS, HTML, Javascript.
+* It is a responsive web page.
+* It has one file for HTML, one for CSS and one for Javascript.
+* It has only one functionality i.e. "Show" and "Hide" some content after clicking on "Show More" and "Show Less" buttun respectively.
+
+## Web Page Performance:
+* I checked  performance of the web page on pagespeed.dev and below is the result.
+
+  ![alt](./Images/pagePerformance.png)
+
+## Deployed Link:
+* I have deployed this application on Netlify. Below is the deployed link.
+
+* https://656ec1922a2b2e1c13e700b7--genuine-sorbet-b0f3b5.netlify.app/
+
--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hi');
+
+function handleShowMore() {
+   
+    var dotSpan = document.getElementById("dot-span");
+    var p2 = document.getElementById("place-desc-p2");
+    var showMoreSpan = document.getElementById("more-desc-span");
+    var showMoreBtn = document.getElementById("show-more-btn");
+    
+    if(dotSpan.style.display==='none') {
+        dotSpan.style.display='inline';
+        showMoreSpan.style.display='none';
+        p2.style.display='none';
+        showMoreBtn.innerHTML = '<span style="margin-right: 5px;">Show More</span><img src="./Images/bigdownarrow.png" alt="bigdownarrow" height="20px">';
+    } else {
+        dotSpan.style.display='none';
+        showMoreSpan.style.display='inline';
+        p2.style.display='block';
+        showMoreBtn.innerHTML = '<span style="margin-right: 5px;">Show Less</span><img src="./Images/biguparrow.png" alt="bigdownarrow" height="20px">';
+    }
+}
